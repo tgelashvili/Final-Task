7786b6444b974ae0acdbeff06342f4b5257bb5b9
tgelashvili <taak.gelashvili@gmail.com>
2021-12-03 23:46:44 +0400

    skill section

--- app.js
+++ app.js
@@ -65,3 +65,25 @@ window.addEventListener("scroll", () => {
 		hideProgress();
 	}
 })
+
+
+// skills section 
+
+var acc = document.getElementsByClassName("front-service");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("mouseenter", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
