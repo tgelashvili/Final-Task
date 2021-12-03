5076c03ca914d6a5ec0efa3ae5e3b9865371f01d
tgelashvili <taak.gelashvili@gmail.com>
2021-12-03 22:19:45 +0400

    second section completed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// image slider
+
 var slideIndex = 0;
 showSlides();
 
@@ -17,3 +19,49 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+// progress bar
+
+// var i = 0;
+// document.addEventListener ('scroll', function move() {
+//     if (i == 0) {
+//     i = 1;
+//     var elem = document.getElementById("myBar");
+//     var width = 1;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//       if (width >= 100) {
+//         clearInterval(id);
+//         i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//       }
+//     }
+//   }
+// });
+
+const skillsSection = document.querySelector(".my-progress-charts");
+const progressBars = document.querySelectorAll(".progress-percent");
+function showProgress() {
+	progressBars.forEach(progressBar => {
+		const value = progressBar.dataset.progress;
+		progressBar.style.opacity = 1;
+		progressBar.style.width = `${value}%`;
+	});
+}
+function hideProgress() {
+	progressBars.forEach(p => {
+		p.style.opacity = 0;
+		p.style.width = 0;
+	});
+}
+window.addEventListener("scroll", () => {
+	const sectionPos = skillsSection.getBoundingClientRect().top;
+	const screenPos = window.innerHeight / 2;
+	if (sectionPos < screenPos) {
+		showProgress();
+	} else {
+		hideProgress();
+	}
+})
